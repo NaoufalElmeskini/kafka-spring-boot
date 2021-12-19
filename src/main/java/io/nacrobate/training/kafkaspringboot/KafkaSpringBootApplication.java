e9afc33146b560727e5a147cf8c2a782bfd9936e
Naoufal <naoufal_el_meskini@carrefour.com>
2021-12-19 04:05:02 +0100

    initial commit

---
+++ src/main/java/io/nacrobate/training/kafkaspringboot/KafkaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.nacrobate.training.kafkaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringBootApplication.class, args);
+    }
+
+}
